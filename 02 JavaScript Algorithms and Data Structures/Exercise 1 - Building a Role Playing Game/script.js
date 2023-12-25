3c75e5eaa795bf14eed2f42c6a646ba8a4c0ef95
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-12-25 16:11:54 +0300

    Building a Role Playing Game - Step 59

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
@@ -22,6 +22,11 @@ const locations = [
     name: "town square",
     "button text": ["Go to store", "Go to cave", "Fight dragon"],
     text: "You are in the town square. You see a sign that says \"Store\"."
+  }, {
+    "button functions": [buyHealth, buyWeapon, goTown],
+    name: "store",
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    text: "You enter the store."
   }
 ];
 
