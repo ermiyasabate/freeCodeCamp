867524b80fc3069f534edbab398b03abaf240c82
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-12-25 17:55:34 +0300

    Building a Role Playing Game - Step 90

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
@@ -105,7 +105,7 @@ function buyWeapon() {
     gold -= 30;
     currentWeapon++;
     goldText.innerText = gold;
-    let newWeapon = weapons[currentWeapon];
+    let newWeapon = weapons[currentWeapon].name;
 
     text.innerText = "You now have a new weapon.";
   }
