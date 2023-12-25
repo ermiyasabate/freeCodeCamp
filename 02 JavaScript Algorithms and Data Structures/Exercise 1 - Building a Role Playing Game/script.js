c64c700859932781dbb881ee398f7b95e35fc280
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-12-25 14:26:06 +0300

    Building a Role Playing Game - Step 41

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
@@ -25,3 +25,11 @@ button3.onclick = fightDragon;
 function goStore() {
     console.log("Going to store.");
 }
+
+function goCave() {
+    console.log("Going to cave.");
+}
+
+function goStore() {
+    console.log("Going to store.");
+}
