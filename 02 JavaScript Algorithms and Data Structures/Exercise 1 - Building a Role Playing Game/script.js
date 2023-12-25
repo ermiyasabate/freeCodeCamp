0bec3fac2e44235b0163fa062e72d2b5b19153db
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-12-25 18:30:32 +0300

    Building a Role Playing Game - Step 99

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
@@ -114,6 +114,8 @@ function buyWeapon() {
     } else {
       text.innerText = "You do not have enough gold to buy a weapon.";
     }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
   }
 }
 
