77e2c3b31309708e92a334eb2e008abf8a8e19b4
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-12-25 13:38:49 +0300

    Building a Role Playing Game - Step 36

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
@@ -3,17 +3,18 @@ let health = 100;
 let gold = 50;
 let currentWeapon = 0;
 let fighting;
-let monsterHealth ;
+let monsterHealth;
 let inventory = ["stick"];
 
-const button1 = document.querySelector("#button1");
+const button1 = document.querySelector('#button1');
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
-
 const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText");
 const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
-const monsterHealthText = document.querySelector("#monsterHealth");
+const monsterHealthText =document.querySelector("#monsterHealth");
+
+//initialize buttons
