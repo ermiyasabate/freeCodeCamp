e3217e51ee19394acdb390800daccf6746a9424f
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-12-25 15:08:28 +0300

    Building a Role Playing Game - Step 51

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
@@ -22,6 +22,10 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function update(location) {
+  
+}
+
 function goTown() {
   button1.innerText = "Go to store";
   button2.innerText = "Go to cave";
