125b591dcf29d259a2346d29410edb170eeb6867
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-12-25 18:23:18 +0300

    Building a Role Playing Game - Step 96

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
@@ -101,17 +101,19 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-  if (gold >= 30) {
-    gold -= 30;
-    currentWeapon++;
-    goldText.innerText = gold;
-    let newWeapon = weapons[currentWeapon].name;
+  if (currentWeapon < 3) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
 
-    text.innerText = "You now have a " + newWeapon + ".";
-    inventory.push(newWeapon);
-    text.innerText += " In your inventory you have: " + inventory;
-  } else {
-    text.innerText = "You do not have enough gold to buy a weapon.";
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
   }
 }
 
