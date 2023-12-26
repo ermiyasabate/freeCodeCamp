2596d9c5750dadeda2c90d7f582cbc400e5dae2c
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-12-26 11:03:13 +0300

    Building a Role Playing Game - Step 107

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
@@ -128,6 +128,7 @@ function sellWeapon() {
     let currentWeapon;
     currentWeapon = inventory.shift();
     text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
   }
 }
 
