73687832ed003aa7503ea27e0c4102ada95eb3ed
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-12-30 11:43:23 +0300

    Building a Role Playing Game - Step 137

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
@@ -192,3 +192,17 @@ function defeatMonster() {
 function lose() {
   update(locations[6]);
 }
+
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeapon = 0;
+  inventory = ["stick"];
+
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+
+  goTown();
+}
