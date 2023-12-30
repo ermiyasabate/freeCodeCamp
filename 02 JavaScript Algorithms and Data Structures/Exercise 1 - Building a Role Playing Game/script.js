74c049edf10d30b64ce78ee754c8e7c5e248bdc6
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-12-30 10:58:27 +0300

    Building a Role Playing Game - Step 122

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
@@ -159,5 +159,7 @@ function goFight() {
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeapon].power;
 }
 function dodge() { }
