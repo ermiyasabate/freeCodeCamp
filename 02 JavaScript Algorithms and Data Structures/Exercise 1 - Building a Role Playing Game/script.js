eaf5d806a7cc76e038dbece700a476bbb7708b11
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-12-30 17:46:47 +0300

    Building a Role Playing Game - Step 166

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
@@ -257,7 +257,7 @@ function pick(guess) {
   while (numbers.length < 10) {
     numbers.push(Math.floor(Math.random() * 11));
   }
-  text.innerText = "You picked " + guess + ". Here are the random numbers:";
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
 }
 
 function pickTwo() {
