70a8aafe00124a9a6976fca0b587f16a967969f8
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-12-30 18:21:24 +0300

    Building a Role Playing Game - Step 175

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Role Playing Game/script.js
@@ -268,6 +268,9 @@ function pick(guess) {
       text.innerText += "Wrong! You lose 10 health!";
       health -= 10;
       healthText.innerText = health;
+      if(health <= 0) {
+        lose();
+      }
     }
     text.innerText += numbers[i] + "\n";
   }
