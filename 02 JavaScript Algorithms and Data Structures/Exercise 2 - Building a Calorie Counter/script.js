d677d0c39088cdf2061f47244ca9e2d9cf8de513
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-12-31 23:06:20 +0300

    Building a Calorie Counter - Step 16

--- 02 JavaScript Algorithms and Data Structures/Exercise 2 - Building a Calorie Counter/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 2 - Building a Calorie Counter/script.js
@@ -1,3 +1,6 @@
 const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
