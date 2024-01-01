8911b56dcc5ad3c6a993daa2d75ba54c24a09a1e
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-01-01 12:58:29 +0300

    Building a Calorie Counter - Step 39

--- 02 JavaScript Algorithms and Data Structures/Exercise 2 - Building a Calorie Counter/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 2 - Building a Calorie Counter/script.js
@@ -24,5 +24,5 @@ function isInvalidInput(str) {
 
 function addEntry() {
     const targetId = '#' + entryDropdown.value;
-    const targetInputContainer = document.querySelector(targetId + ' .input-container');
+    const targetInputContainer = document.querySelector(`${targetId} .input-container`);
 }
