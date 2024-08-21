764a7b73ec4fc2001028f166435241218b4c681f
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-08-21 12:45:42 +0300

    freeCodeCamp - JavaScript Algorithms and Data Structures - Step 24

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Pyramid Generator/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Pyramid Generator/script.js
@@ -5,4 +5,11 @@ let rows = ["Naomi", "Quincy", "CamperChan"];
 // console.log(rows[0])
 
 // rows[rows.length - 1] = 10
+
+let cities = ["London", "New York", "Mumbai"];
+console.log(cities);
+
+cities[cities.length - 1] = "Mexico City";
+console.log(cities);
+
 console.log(rows)
