96259ea161a60e796e1effff0213b1a90f876207
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-08-21 12:42:03 +0300

    freeCodeCamp - JavaScript Algorithms and Data Structures - Step 23

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Pyramid Generator/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Pyramid Generator/script.js
@@ -2,7 +2,7 @@ let character = 'Hello';
 let count = 8;
 
 let rows = ["Naomi", "Quincy", "CamperChan"];
-console.log(rows[0])
+// console.log(rows[0])
 
-rows[rows.length - 1] = 10
+// rows[rows.length - 1] = 10
 console.log(rows)
