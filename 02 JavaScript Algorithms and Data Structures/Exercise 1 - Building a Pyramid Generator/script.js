81b7c2b9a68a7b269431053b31e2a93cfa0e98d2
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-27 10:26:05 +0300

    freeCodeCamp - JavaScript - Building a Pyramid Generator - Step 12

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Pyramid Generator/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Pyramid Generator/script.js
@@ -1,10 +1,1 @@
 let character = 'Hello';
-console.log(character);
-
-character = 'World';
-console.log(character);
-
-let secondCharacter;
-// secondCharacter = 'Test';
-secondCharacter = character;
-console.log(secondCharacter);
