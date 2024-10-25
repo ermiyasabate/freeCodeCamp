2bbf45383adfc4c7217d48ea9ed0b910a7fa4e8e
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-10-25 15:01:18 +0300

    freeCodeCamp - JavaScript - Building a Pyramid Generator - Step 24

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Pyramid Generator/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Pyramid Generator/script.js
@@ -1,4 +1,10 @@
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
+
+let cities = ["London", "New York", "Mumbai"]
+console.log(cities);
+cities[cities.length - 1] = "Mexico City"
+console.log(cities);
+
 console.log(rows);
