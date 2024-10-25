90cb35afe25e057df391c9642cf1a3e7170531e9
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-10-25 16:11:44 +0300

    freeCodeCamp - JavaScript - Building a Pyramid Generator - Step 27

--- 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Pyramid Generator/script.js
+++ 02 JavaScript Algorithms and Data Structures/Exercise 1 - Building a Pyramid Generator/script.js
@@ -2,4 +2,8 @@ let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
 rows.push("freeCodeCamp")
+
+let popped = rows.pop()
+console.log(popped);
+
 console.log(rows);
